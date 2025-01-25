bc4c0c4ca0cb436d4882197830f0a116452e8977
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-01-25 11:16:57 +0530

    Add Edit btn

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -13,10 +13,16 @@ export const todoSlice = createSlice({
         },
         removeTodo: (state, action) => {
             state.todos = state.todos.filter(todo => todo.id !== action.payload)
+        },
+        editTodo: (state, action) => {
+            const todo = state.todos.find(todo => todo.id === action.payload.id)
+            if(todo) {
+                todo.text = action.payload.text
+            }
         }
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const {addTodo, removeTodo,editTodo} = todoSlice.actions
 
 export default todoSlice.reducer
