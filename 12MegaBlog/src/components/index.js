9e82ed72f8af4ea01751180258ccf9d1eb9e6078
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-02-05 12:33:17 +0530

    add Authentication and functional card

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,17 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container';
 import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn'
+import Button from './Button'
+import PostCard from './PostCard'
+import Select from './Select'
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    PostCard,
+    Select
 }
