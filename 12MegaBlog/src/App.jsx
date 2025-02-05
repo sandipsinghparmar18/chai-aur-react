9b2a49190de8ec67a36ef2861d036866678ff033
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-02-05 11:06:27 +0530

    professional Components design

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth.js'
+import {login,logout} from './store/authSlice.js'
+import {Header, Footer } from './components/index.js'
+import {Outlet } from 'react-router-dom'
 
 function App() {
+  const [loading,setLoading]=useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+        if(userData){
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout())
+        }
+    })
+    .finally(()=> setLoading(false))
+  },[])
   
-  return (
-    <>
-      <h1>A blog with app write</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between
+     bg-gray-400' >
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO: {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
